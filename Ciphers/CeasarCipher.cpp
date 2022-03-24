de4b223c35732f43d43f0ccaf496a761d5446a2d
saneenKP <saneenkp2000@gmail.com>
2022-03-24 16:23:34 +0530

    Ceasar cihper implementation done

--- Ciphers/CeasarCipher.cpp
+++ Ciphers/CeasarCipher.cpp
@@ -1,5 +1,23 @@
 #include<iostream>
 
+std::string ceasarCipher(std::string &message){
+
+    int cipherElementPos = 0;
+    for (int i = 0; i < message.length(); ++i){
+
+        int shift = (message[i] + 3);
+        if (message[i] > 90)
+            cipherElementPos = ( (shift > 122) ? (shift - 122) : shift);
+            
+        if (message[i] < 90)
+            cipherElementPos = ( (shift > 90) ? (shift - 90) : shift);
+        
+        message[i] = cipherElementPos;
+    }
+    
+    return message;
+}
+
 int main(){
     std::string message;
     std::string cipher;
