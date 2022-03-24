198c7bed61b2d74bea6c348f1eb9f602ae2c16c5
saneenKP <saneenkp2000@gmail.com>
2022-03-24 22:13:41 +0530

    mono alphabetic cipher main function done

---
+++ Ciphers/MonoAlphabeticCipher.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+int main(){
+    std::string message;
+    std::string cipher;
+
+    std::cout<<"Enter the message : ";
+    std::cin>>message;
+
+    cipher = monoalphabeticCipher(message);
+    std::cout<<"Cipher message = "<<cipher<<"\n";
+
+}
