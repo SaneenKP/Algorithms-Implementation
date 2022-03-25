3511f377c53069653ec4c8a86edec2ac1b8b5431
saneenKP <saneenkp2000@gmail.com>
2022-03-26 02:21:42 +0530

    distribution of random numbers for small letters also implemented

--- Ciphers/MonoAlphabeticCipher.cpp
+++ Ciphers/MonoAlphabeticCipher.cpp
@@ -7,12 +7,24 @@ std::unordered_map<int , int> randomCharacterOrderGenerator(){
     std::unordered_map<int,int> randomCharactersPositions;
     std::random_device seed;
     std::mt19937 gen{seed()};
-    std::uniform_int_distribution dist{65 , 90};
+    std::uniform_int_distribution distributionForCapital{65 , 90};
+    std::uniform_int_distribution distributionForSmall{97 , 122};
 
     int alphabets = 65;
     while (alphabets <= 90)
     {
-        int randomAlphabet = dist(gen);
+        int randomAlphabet = distributionForCapital(gen);
+        if (randomCharactersPositions.find(randomAlphabet) == randomCharactersPositions.end())
+        {
+            randomCharactersPositions[randomAlphabet] = alphabets;
+            alphabets++;
+        }
+    }
+
+    alphabets = 97;
+    while (alphabets <= 122)
+    {
+        int randomAlphabet = distributionForSmall(gen);
         if (randomCharactersPositions.find(randomAlphabet) == randomCharactersPositions.end())
         {
             randomCharactersPositions[randomAlphabet] = alphabets;
@@ -27,9 +39,12 @@ std::unordered_map<int , int> randomCharacterOrderGenerator(){
 std::string monoalphabeticCipher(std::string &message){
 
     std::unordered_map<int , int> characterMap = randomCharacterOrderGenerator();
-    for(auto x : characterMap){
-        std::cout<<x.first<<"   "<<x.second<<std::endl;
-    }
+    // for (int i = 0; i < message.length(); i++)
+    // {
+    //     std::cout<<characterMap[(int)message[i]]<<std::endl;
+    // }
+
+    std::cout<<characterMap.size();
     
     return message;
 
