44e9b8b6f8f6a18c7b9f8dd72e24865ce8a44cff
saneenKP <saneenkp2000@gmail.com>
2022-03-27 00:34:24 +0530

    playfair cipher function created

--- Ciphers/playFairCipher.cpp
+++ Ciphers/playFairCipher.cpp
@@ -1,13 +1,24 @@
 #include<iostream>
 
+std::string playFairCipher(std::string &message , std::string key){
+
+
+
+}
+
 int main(){
     std::string message;
     std::string cipher;
+    std::string key;
 
     std::cout<<"Enter the message : ";
     std::cin>>message;
 
-    cipher = monoalphabeticCipher(message);
-    std::cout<<"Cipher message = "<<cipher<<"\n";
+    std::cout<<"\nEnter key : ";
+    std::cin>>key;
+
+    cipher = playFairCipher(message , key);
+
+    std::cout<<"\n Cipher Text = "<<cipher<<std::endl;
 
 }
