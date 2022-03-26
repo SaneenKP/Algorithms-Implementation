3dfdffee320ac5517ee11350635b326a112971f0
saneenKP <saneenkp2000@gmail.com>
2022-03-27 01:20:59 +0530

    alphabet occurence method done according to capital and small letters

--- Ciphers/playFairCipher.cpp
+++ Ciphers/playFairCipher.cpp
@@ -10,11 +10,53 @@ void display(std::vector<std::vector<char>> &keyMatrix){
    }
 }
 
+std::vector<std::vector<char>> fillKeyMatrix(std::vector<std::vector<char>> &keyMatrix , std::string key){
+
+   bool keyalphabetsoccurrence[26]{false};
+
+   for (int i = 0; i < key.length(); ++i){
+       if (key[i] <= 92)
+           keyalphabetsoccurrence[key[i] - 65] = true;
+       if (key[i] >= 97)
+           keyalphabetsoccurrence[key[i] - 97] = true;
+   }
+    
+    for (int i = 0; i < 26; ++i)
+    {
+        std::cout<<keyalphabetsoccurrence[i]<<std::endl;
+    }
+    
+   
+//    int keyCount = 0;
+//    for(auto x  : keyMatrix){
+//        for(auto y : x){
+//            if (keyCount < key.length())
+//            {
+//                 if (!keyalphabetsoccurrence[y])
+//                 {
+//                     y = key[keyCount];
+//                     keyalphabetsoccurrence[y] = true;
+//                     keyCount++;
+//                 }
+//            }else{
+
+//            }
+           
+//        }
+//    }
+
+
+    return keyMatrix;
+
+}
+
 std::string playFairCipher(std::string &message , std::string key){
 
 
     std::vector<std::vector<char>> keyMatrix(5 , std::vector<char>(5));
-    display(keyMatrix);
+    keyMatrix = fillKeyMatrix(keyMatrix , key);
+    
+
 
     return message;
 
