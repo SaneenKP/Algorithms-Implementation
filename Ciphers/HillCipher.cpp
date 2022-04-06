04afe354185e3b225853ba71e7482eb42b732f0d
saneenKP <saneenkp2000@gmail.com>
2022-04-07 01:35:08 +0530

    hill cipher main and remove white space function done

---
+++ Ciphers/HillCipher.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+#include<algorithm>
+
+std::string removeWhiteSpace(std::string message){
+
+    std::string finalMessage = "";
+    for (int i = 0; i < message.length(); ++i)
+    {
+        if (message[i] == ' ')
+            continue;
+        finalMessage += message[i];
+    }
+    return finalMessage; 
+}
+
+int main(){
+       
+    std::string message;
+    std::string cipher;
+    std::string key;
+
+    std::cout<<"Enter the message : ";
+    getline(std::cin , message);
+
+    message = removeWhiteSpace(message);
+
+    std::cout<<"\nEnter key : ";
+    std::cin>>key;
+
+    std::transform(key.begin() , key.end() , key.begin() , ::toupper);
+    std::transform(message.begin() , message.end() , message.begin() , ::toupper);
+
+    cipher = HillCipher(message , key);
+
+    std::cout<<"\nCipher Text = "<<cipher<<std::endl;
+}
