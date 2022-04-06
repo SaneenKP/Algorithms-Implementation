e8f6a8a784d954717a586b9a1fd4db557abba510
saneenKP <saneenkp2000@gmail.com>
2022-04-07 01:58:27 +0530

    Hill cipher generate random key function done

--- Ciphers/HillCipher.cpp
+++ Ciphers/HillCipher.cpp
@@ -1,16 +1,39 @@
 #include<iostream>
 #include<algorithm>
 #include<vector>
+#include<random>
 
 std::vector<std::vector<int>> generateKey(int termFrequency){
+
     std::vector<std::vector<int>> keyMatrix(termFrequency,std::vector<int>(termFrequency,0));
+
+    std::random_device seed;
+    std::mt19937 gen{seed()};
+    std::uniform_int_distribution distributionForKey{1,99};
+
+    for (int i = 0; i < termFrequency; ++i)
+    {
+        for (int j = 0; j < termFrequency; ++j)
+        {
+            keyMatrix[i][j] = distributionForKey(gen);
+        }
+    }
+
+    return keyMatrix; 
+
 }
 
-std::string hillCipher(std::string &message , int termFrequency){
+std::string hillCipher(std::string &message , int &termFrequency){
 
     std::vector<std::vector<int>> keyMatrix = generateKey(termFrequency);
     
-
+    for(auto x : keyMatrix){
+        for(auto y : x){
+            std::cout<<y<<" ";
+        }
+        std::cout<<"\n";
+    }
+    return "";
 }
 
 std::string removeWhiteSpace(std::string message){
@@ -29,20 +52,19 @@ int main(){
        
     std::string message;
     std::string cipher;
-    std::string key;
+    int termFrequency;
 
     std::cout<<"Enter the message : ";
     getline(std::cin , message);
 
+    std::cout<<"Enter the termFrequency : ";
+    std::cin>>termFrequency;
+
     message = removeWhiteSpace(message);
 
-    std::cout<<"\nEnter key : ";
-    std::cin>>key;
-
-    std::transform(key.begin() , key.end() , key.begin() , ::toupper);
     std::transform(message.begin() , message.end() , message.begin() , ::toupper);
 
-    cipher = hillCipher(message);
+    cipher = hillCipher(message , termFrequency);
 
     std::cout<<"\nCipher Text = "<<cipher<<std::endl;
 }
