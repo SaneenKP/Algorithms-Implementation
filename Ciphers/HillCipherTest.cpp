9784c4f39f4b32ab6568249eabbfddc9945ab042
saneenKP <saneenkp2000@gmail.com>
2022-04-23 00:54:26 +0530

    main function updated

--- Ciphers/HillCipherTest.cpp
+++ Ciphers/HillCipherTest.cpp
@@ -2,18 +2,58 @@
 
 class HillCipher{
 
+    private:
     std::string message;
     std::string cipher;
     int termFrequency;
+
+    public: 
+
+    
+
+
+    //getters and setters
+    void setMessage(std::string value){
+        message = value;
+    }
+    void setCipher(std::string value){
+        cipher = value;
+    }
+    void setTermFrequency(int value){
+        termFrequency = value;
+    }
+
+    std::string getMessage(){
+        return message;
+    } 
+
+    std::string getCipher(){
+        return cipher;
+    }
+
+    int getTermFrequency(){
+        return termFrequency;
+    }
 
 
 };
 
 int main(){
 
+    std::string message;
+    std::string cipher;
+    int termFrequency;
+
+    HillCipher hillcipher;
     std::cout<<"Enter the message : ";
     getline(std::cin , message);
 
+    hillcipher.setMessage(message);
+
     std::cout<<"Enter the termFrequency : ";
     std::cin>>termFrequency;
+
+    hillcipher.setTermFrequency(termFrequency);
+
+
 }
