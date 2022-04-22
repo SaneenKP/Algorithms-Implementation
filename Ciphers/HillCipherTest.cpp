40f6f2f19443f4c839c29033124c1a36f2b9c056
saneenKP <saneenkp2000@gmail.com>
2022-04-23 00:50:02 +0530

    getters and setters created for private variables

--- Ciphers/HillCipherTest.cpp
+++ Ciphers/HillCipherTest.cpp
@@ -2,17 +2,14 @@
 
 class HillCipher{
 
+    std::string message;
+    std::string cipher;
+    int termFrequency;
 
-    void transformMessage(std::string &message){
-        
-    }
 
 };
 
 int main(){
-    std::string message;
-    std::string cipher;
-    int termFrequency;
 
     std::cout<<"Enter the message : ";
     getline(std::cin , message);
